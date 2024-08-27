cade598553ce4d9707af74f63459f3e31dc8f009
Daniel Chacon <devdc.tech@gmail>
2024-08-27 11:43:21 -0700

    Adding PHP

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
+<?php
+    $title = "Graphic";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Graphic</title>
-
+    <title><?= $title ?></title>
 </head>
 <body>
-
-
-<script src="graphic.js"></script>
+    <script src="graphic.js"></script>
 </body>
 </html>
